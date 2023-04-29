6700758d94202e2ebf11bfb18f72f0d697e66981
jeff bernardis <jeff.bernardis@gmail.com>
2023-04-29 18:07:54 -0400

    check in

--- src/install.bat
+++ src/install.bat
@@ -1,4 +1,6 @@
 mkdir logs
+mkdir output
 python -m venv ./venv
+.\venv\Scripts\python -m pip install wheel
 .\venv\Scripts\python -m pip install -r requirements.txt
 .\venv\Scripts\python config\main.py
